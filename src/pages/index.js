288b7fe9f9d3adfe00554ebe7627739ad1c7734e
Takio Yamaoka <y.takio@gmail.com>
2021-03-28 21:58:03 +0900

    update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div style={{ color: `purple` }}>
       <Link to="/contact/">Contact</Link>
-      <Header headerText="Hello Gatsby!" />
+      <Header headerText="こんにちわ Gatsby!" />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
