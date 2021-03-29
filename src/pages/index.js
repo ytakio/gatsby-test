f72ee7a4e5a0c823d4129ef3ce6ad00c94ca8eff
Takio Yamaoka <y.takio@gmail.com>
2021-03-29 12:06:00 +0900

    こんにちわ世界

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div style={{ color: `purple` }}>
       <Link to="/contact/">Contact</Link>
-      <Header headerText="こんにちわ Gatsby!" />
+      <Header headerText="こんにちわ 世界!" />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
